53da42cb1b87de03094188e13fb29af61c5627ce
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-03-14 01:04:13 +0530

    Initial

--- deployment/build.sh
+++ deployment/build.sh
@@ -3,5 +3,5 @@
 docker build -t clean-email-image:prd -f Dockerfile .
 if [ `echo $?` == 0 ]; then
 	docker rm -f clean-email
-	docker run -dt --restart=always -p 8004:8004 --name clean-email clean-email-image:prd
+	docker run -dt --restart=always -p 8005:8005 --name clean-email clean-email-image:prd
 fi
